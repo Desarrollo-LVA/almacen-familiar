983a682682c0f9f64deffe8484089ca8ea622d42
José Angel Leyva Portilla <joseangel.leyva@outlook.com>
2017-08-15 20:28:36 -0500

    Primer esquema de base de datos

---
+++ esquema.sql
@@ -0,0 +1,34 @@
+--Diseño de base de datos
+create table articulos
+  (
+    articuloid serial primary key,
+    nombre text
+  );
+
+create table lugar
+  (
+    lugarid serial primary key,
+    nombre text
+  );
+
+create table compra
+  (
+    compraid serial primary key,
+    lugarid integer references lugar(lugarid)
+  );
+
+create table unidad
+  (
+    unidadid serial primary key,
+    nombre text
+  );
+
+create table detalle
+  (
+    detalleid serial primary key,
+    compraid integer references compra(compraid),
+    articuloid integer references articulos(articuloid),
+    unidadid integer references unidad(unidadid),
+    cantidad numeric,
+    precio numeric
+  );
